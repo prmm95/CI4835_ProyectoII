f6676f6af44c43507e94a1e42b2cafb33d6b20de
Pablo Maldonado <prmm95@gmail.com>
2016-06-20 11:46:00 -0400

    se empezo a trabajar en la funcion para calcular los costos del estacionamiento

---
+++ main.c
@@ -0,0 +1,27 @@
+/*
+ * Archivo: main.c
+ * 
+ * Descripción: 
+ *
+ * Autores:
+ *	 Samuel Arleo, 10-10969.
+ *   Pablo Maldonado, 12-10561.
+ *
+ * Última fecha de modificación: 27/06/2016
+ *
+*/
+
+ #include <stdio.h>
+
+int calcular_costo(int horaE, int minutE, int horaS, int minutS) {
+
+
+
+}
+
+
+ int main() {
+
+
+ 	return(0);
+ }
