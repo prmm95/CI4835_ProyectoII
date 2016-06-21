83ff2d8a407cae751739793f6f7ccd26618fbccd
Pablo Maldonado <prmm95@gmail.com>
2016-06-21 22:51:04 +0000

    Se agrego la funcion para escribir un log en una bitacora

--- main.c
+++ main.c
@@ -43,13 +43,49 @@ typedef struct tm Tiempo;
 
 //----------------------------------------------------------------------------//
 
- typedef struct operacion {
- 	int tipo;
- 	int fecha; // no es un int. Buscar como manejar fechas en C.
- 	int hora; // no es un int. Buscar tambien como se manejan horas en C.
- 	int codigoVehiculo;
- 	int montoPagado;
- } Operacion;
+ typedef struct vehiculo {
+ 	char *tipoOperacion; 
+ 	Tiempo fechaEntrada;
+ 	Tiempo fechaSalida;
+ 	int codigo;
+ 	int serial; // cambiar, porque no es int.
+ 	int tarifa;
+ } Vehiculo;
+
+//----------------------------------------------------------------------------//
+//                          Definición de funciones                           //
+//----------------------------------------------------------------------------//
+
+// Notas:
+// borrar de la lista al vehiculo cuando sale
+// el contador del codigo es global
+
+void escribirBitacora(char *rutaBitacora,Vehiculo vehiculo) {
+
+	FILE *bitacora;
+	bitacora = fopen(rutaBitacora,"a"); // no se si pasar esto como parametro para no abrir y cerrar el df cada vez
+	Tiempo fechaB;
+	
+	// No se si este if se puede hacer mas elegante:
+	if (vehiculo.tipoOperacion == "Entrada") {
+		fechaB = vehiculo.fechaEntrada;
+	}
+	
+	else {
+		fechaB = vehiculo.fechaSalida;
+	}
+	
+	
+	fprintf(bitacora,"-------------------\n");
+	fprintf(bitacora,"Fecha: %d/%02d/%d \n",fechaB.tm_mday,fechaB.tm_mon + 1,fechaB.tm_year + 1900);
+	fprintf(bitacora,"Hora: %d:%d:%d\n", fechaB.tm_hour, fechaB.tm_min, fechaB.tm_sec);
+	fprintf(bitacora,"Serial: %d \n",vehiculo.serial);
+	fprintf(bitacora,"Código: %d",vehiculo.codigo);
+	fprintf(bitacora,"\n-------------------\n");
+	
+	close(bitacora);
+	
+}
 
 //----------------------------------------------------------------------------//
 
@@ -101,13 +137,27 @@ int main() {
 	tarifaVehiculo = calcular_costo(tm1,tm2);
 	printf("La tarifa a pagar es %d \n",tarifaVehiculo);
 
-	// Formato de impresión para el cliente al momento de entrara al 
-	// estacionamiento:
+	// Formato de impresión para el cliente al momento de entrar al 
+	// estacionamiento: (esto lo haria el cliente)
 	printf("-------------------\n");
 	printf("Código: %d \n",1);
 	printf("Fecha: %d/%d/%d \n",tm1.tm_mday,tm1.tm_mon + 1,tm1.tm_year + 1900);
 	printf("Hora: %d:%d:%d", tm1.tm_hour, tm1.tm_min, tm1.tm_sec);
 	printf("\n-------------------\n");
+	
+	// Prueba
+	Vehiculo carro1;
+	carro1.tipoOperacion = "Entrada";
+	carro1.fechaEntrada = tm1;
+ 	carro1.fechaSalida = tm2;
+ 	carro1.codigo = 123;
+ 	carro1.serial = 456;
+ 	carro1.tarifa = 0;
+	char *rutaBitacora;
+	rutaBitacora = "hola";
+	
+	// antes de salir, hay que calcular tarifa.
+	escribirBitacora(rutaBitacora,carro1);
 
  	return(0);
 }
