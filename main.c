e66ded4eac3543dd17f24576ff7b7db1f5e6809d
Pablo Maldonado <prmm95@gmail.com>
2016-06-22 01:36:07 +0000

    Calcula la tarifa a cobrar por un vehiculo en el estacionamiento

--- main.c
+++ main.c
@@ -25,6 +25,7 @@
 //----------------------------------------------------------------------------//
 
 #include <stdio.h> // Uso de la entrada/salida estándar (I/O).
+#include <math.h>
 #include <time.h>  //
 #include <errno.h> // Uso de la función perror()
 
@@ -43,10 +44,17 @@ typedef struct tm Tiempo;
 
 //----------------------------------------------------------------------------//
 
+typedef struct tiempo {
+	Tiempo tiempoF;
+	time_t segundos;
+} TiempoV;
+
+//----------------------------------------------------------------------------//
+
  typedef struct vehiculo {
  	char *tipoOperacion; 
- 	Tiempo fechaEntrada;
- 	Tiempo fechaSalida;
+ 	TiempoV Entrada;
+ 	TiempoV Salida;
  	int codigo;
  	int serial; // cambiar, porque no es int.
  	int tarifa;
@@ -68,17 +76,17 @@ void escribirBitacora(char *rutaBitacora,Vehiculo vehiculo) {
 	
 	// No se si este if se puede hacer mas elegante:
 	if (vehiculo.tipoOperacion == "Entrada") {
-		fechaB = vehiculo.fechaEntrada;
+		fechaB = vehiculo.Entrada.tiempoF;
 	}
 	
 	else {
-		fechaB = vehiculo.fechaSalida;
+		fechaB = vehiculo.Salida.tiempoF;
 	}
 	
 	
 	fprintf(bitacora,"-------------------\n");
 	fprintf(bitacora,"Fecha: %d/%02d/%d \n",fechaB.tm_mday,fechaB.tm_mon + 1,fechaB.tm_year + 1900);
-	fprintf(bitacora,"Hora: %d:%d:%d\n", fechaB.tm_hour, fechaB.tm_min, fechaB.tm_sec);
+	fprintf(bitacora,"Hora: %02d:%02d:%02d\n", fechaB.tm_hour, fechaB.tm_min, fechaB.tm_sec);
 	fprintf(bitacora,"Serial: %d \n",vehiculo.serial);
 	fprintf(bitacora,"Código: %d",vehiculo.codigo);
 	fprintf(bitacora,"\n-------------------\n");
@@ -87,9 +95,11 @@ void escribirBitacora(char *rutaBitacora,Vehiculo vehiculo) {
 	
 }
 
+
 //----------------------------------------------------------------------------//
 
-int calcular_costo(Tiempo Entrada, Tiempo Salida) {
+
+int calcular_costo(Vehiculo vehiculo) {
 	/*
 	 * Descripción: Esta función determina el costo a pagar de un vehículo por
 	 * su estadía en el estacionamiento. Considere que la primera hora de 
@@ -103,17 +113,22 @@ int calcular_costo(Tiempo Entrada, Tiempo Salida) {
 	 *
 	*/
 
-	// Inicialización de la tarifa
-	int tarifa = 0;
-
-	// Cobro de la primera hora;
-	if (Salida.tm_sec - Entrada.tm_sec > 0) {
-		tarifa += 80;
-	}
-
+	// Inicialización de variables:
+	int tarifa = 80;
+	double segundos;
+	double horas;
+	segundos = difftime(vehiculo.Salida.segundos,vehiculo.Entrada.segundos);
+	horas = ceil(segundos / 3600) - 1;
+	printf("El numero de segundos es %f\n",segundos);
+	printf("El numero de horas es %f\n",horas);
+	tarifa += horas*30;
+	
+	printf("La tarifa es %d\n",tarifa);
+	
 	return tarifa;
 
 }
+
 
 //----------------------------------------------------------------------------//
 //                       Inicio del código principal                          //
@@ -125,31 +140,47 @@ int main() {
  	int puestosOcupados = 0;
  	int tarifaVehiculo;
 
-
-
  	// Se calcula el tiempo actual:
-	time_t t = time(NULL);
-	Tiempo tm1 = *localtime(&t);
-	Tiempo tm2 = *localtime(&t);
+	time_t t1 = time(NULL);
+	Tiempo tm1 = *localtime(&t1);
+	
+	// set beg to the beginning of the month
+    tm1.tm_hour = 0;
+    tm1.tm_min = 0;
+    tm1.tm_sec = 0;
+    tm1.tm_mday = 22;
+	
+	//sleep(10);
+	time_t t2 = t1 + 7201;
+	Tiempo tm2 = *localtime(&t2);
 	// Se muestra en pantalla el tiempo actual:
-	printf("Fecha: %d/%d/%d \n",tm1.tm_mday,tm1.tm_mon + 1,tm1.tm_year + 1900);
-	printf("Hora: %d:%d:%d \n", tm1.tm_hour, tm1.tm_min, tm1.tm_sec);
-	tarifaVehiculo = calcular_costo(tm1,tm2);
-	printf("La tarifa a pagar es %d \n",tarifaVehiculo);
+	printf("Fecha: %02d/%02d/%d \n",tm1.tm_mday,tm1.tm_mon + 1,tm1.tm_year + 1900);
+	printf("Hora: %02d:%02d:%02d \n", tm1.tm_hour, tm1.tm_min, tm1.tm_sec);
+
 
 	// Formato de impresión para el cliente al momento de entrar al 
 	// estacionamiento: (esto lo haria el cliente)
+	//printf("-------------------\n");
+	//printf("Código: %d \n",1);
+	//printf("Fecha: %02d/%02d/%d \n",tm1.tm_mday,tm1.tm_mon + 1,tm1.tm_year + 1900);
+	//printf("Hora: %02d:%02d:%02d", tm1.tm_hour, tm1.tm_min, tm1.tm_sec);
+	//printf("\n-------------------\n");
+	
 	printf("-------------------\n");
-	printf("Código: %d \n",1);
-	printf("Fecha: %d/%d/%d \n",tm1.tm_mday,tm1.tm_mon + 1,tm1.tm_year + 1900);
-	printf("Hora: %d:%d:%d", tm1.tm_hour, tm1.tm_min, tm1.tm_sec);
+	//printf("Código: %d \n",1);
+	printf("Fecha: %02d/%02d/%d \n",tm2.tm_mday,tm2.tm_mon + 1,tm2.tm_year + 1900);
+	printf("Hora: %02d:%02d:%02d", tm2.tm_hour, tm2.tm_min, tm2.tm_sec);
 	printf("\n-------------------\n");
+
+	
 	
 	// Prueba
 	Vehiculo carro1;
 	carro1.tipoOperacion = "Entrada";
-	carro1.fechaEntrada = tm1;
- 	carro1.fechaSalida = tm2;
+	carro1.Entrada.tiempoF = tm1;
+	carro1.Entrada.segundos = t1;
+ 	carro1.Salida.tiempoF = tm2;
+ 	carro1.Salida.segundos = t2;
  	carro1.codigo = 123;
  	carro1.serial = 456;
  	carro1.tarifa = 0;
@@ -158,6 +189,9 @@ int main() {
 	
 	// antes de salir, hay que calcular tarifa.
 	escribirBitacora(rutaBitacora,carro1);
+	
+	tarifaVehiculo = calcular_costo(carro1);
+	//printf("La tarifa a pagar es %d \n",tarifaVehiculo);
 
  	return(0);
 }
