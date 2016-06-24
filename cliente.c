3a94b28ea47647ad9a906bb470b677ea02536028
Pablo Maldonado <prmm95@gmail.com>
2016-06-24 18:16:51 -0400

    se reestructuro un poco el codigo

--- cliente.c
+++ cliente.c
@@ -1,10 +1,21 @@
 /*
-* Ejemplo de cliente de chat simple con datagramas (UDP).
-*
-* Leandro Lucarella - Copyleft 2004
-* Basado en diversos ejemplos públicos.
-*
-*/ 
+ * Archivo: cliente.c
+ * 
+ * Descripción: 
+ *
+ * Autores:
+ *	 Samuel Arleo, 10-10969.
+ *   Pablo Maldonado, 12-10561.
+ *
+ * Última fecha de modificación: 27/06/2016
+ *
+ *
+*/
+
+//----------------------------------------------------------------------------//
+//                        Directivas de preprocesador                         //
+//----------------------------------------------------------------------------//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -13,40 +24,82 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
+
+//----------------------------------------------------------------------------//
+//                          Definición de constantes                          //
+//----------------------------------------------------------------------------//
+
 #define SERVER_PORT 4321
 #define BUFFER_LEN 1024
-int main(int argc, char *argv[]){
+
+//----------------------------------------------------------------------------//
+//                       Inicio del código principal                          //
+//----------------------------------------------------------------------------//
+
+int main(int argc, char *argv[]) {
+
+	char *placa,*modulo,*opcion;
+	int i;
+	long puerto;
+	if (argc < 9){
+		printf("\nUso: sem_cli -d nombre_modulo_atencion -p puerto -c op -i id_vehiculo\n\n");
+		exit(0);
+	}
+	for (i=1;i<argc;i++){ /* Aqui vamos contemplar casos como que corran el cliente con sem_cli hola -d hola ? (Ver enunciado)*/
+		if (strcmp(argv[i],"-p") == 0){
+			puerto = atoll(argv[i+1]);
+		}else if(strcmp(argv[i],"-d") == 0){
+			modulo = malloc(sizeof(argv[i+1]));
+			strcpy(modulo,argv[i+1]);
+		}else if(strcmp(argv[i],"-c") == 0){
+			opcion = malloc(sizeof(argv[i+1]));
+			strcpy(opcion,argv[i+1]);
+		}else if(strcmp(argv[i],"-i") == 0){
+			placa = malloc(sizeof(argv[i+1]));
+			strcpy(placa,argv[i+1]);
+		}
+	}
+	/*printf("PLACA: %s\n", placa);
+	printf("MODULO: %s\n", modulo);
+	printf("OPCION: %s\n", opcion);
+	printf("PUERTO: %ld\n", puerto);*/
+
 	int sockfd; /* descriptor a usar con el socket */
 	struct sockaddr_in their_addr; /* almacenara la direccion IP y numero de puerto del servidor */
 	struct hostent *he; /* para obtener nombre del host */
 	int numbytes; /* conteo de bytes a escribir */
-	if (argc != 3) {
-		fprintf(stderr,"\nuso: %s cliente hostname mensaje\n", argv[0]);
-		exit(1);
-	}
 	/* convertimos el hostname a su direccion IP */
-	if ((he=gethostbyname(argv[1])) == NULL) {
+	if ((he=gethostbyname(modulo)) == NULL) {
 		perror("gethostbyname");
 		exit(1);
 	}
+
 	/* Creamos el socket */
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
 		perror("socket");
 		exit(2);
 	}
+
 	/* a donde mandar */
 	their_addr.sin_family = AF_INET; /* usa host byte order */
 	their_addr.sin_port = htons(SERVER_PORT); /* usa network byte order */
 	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
 	bzero(&(their_addr.sin_zero), 8); /* pone en cero el resto */
+	
 	/* enviamos el mensaje */
-	if ((numbytes=sendto(sockfd,argv[2],strlen(argv[2]),0,(struct sockaddr *)&their_addr,
-		sizeof(struct sockaddr))) == -1) {
+	char *mensaje = "holaaaaa";
+	if ((numbytes=sendto(sockfd,mensaje,strlen(mensaje),0,(struct sockaddr *)&their_addr,
+	sizeof(struct sockaddr))) == -1) {
 		perror("sendto");
 		exit(2);
 	}
+	
 	printf("enviados %d bytes hacia %s\n",numbytes,inet_ntoa(their_addr.sin_addr));
 	/* cierro socket */
 	close(sockfd);
-	exit (0);
+	exit(0);
 }
+
+//----------------------------------------------------------------------------//
+//                         Fin del código principal                           //
+//----------------------------------------------------------------------------//
--- servidor.c
+++ servidor.c
@@ -1,10 +1,22 @@
 /*
-* Ejemplo de server de chat simple con datagramas (UDP).
-*
-* Leandro Lucarella - Copyleft 2004
-* Basado en otros ejemplos públicos.
-*
+ * Archivo: servidor.c
+ * 
+ * Descripción: 
+ *
+ * Autores:
+ *	 Samuel Arleo, 10-10969.
+ *   Pablo Maldonado, 12-10561.
+ *
+ * Última fecha de modificación: 27/06/2016
+ *
+ *
 */
+
+
+//----------------------------------------------------------------------------//
+//                        Directivas de preprocesador                         //
+//----------------------------------------------------------------------------//
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,9 +26,17 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
-#define NUM_THREADS 1
-#define SERVER_PORT 4321
+
+//----------------------------------------------------------------------------//
+//                          Definición de constantes                          //
+//----------------------------------------------------------------------------//
+
+#define NUM_THREADS 3
 #define BUFFER_LEN 1024
+
+//----------------------------------------------------------------------------//
+//                      Definición del tipos estructurados                    //
+//----------------------------------------------------------------------------//
 
 struct Skt
 {
@@ -27,9 +47,12 @@ struct Skt
 	int numbytes;
 }skt;
 
-void *beginProto(void *sockt){
 
-	struct Skt *skt = sockt;
+//----------------------------------------------------------------------------//
+//                          Definición de funciones                           //
+//----------------------------------------------------------------------------//
+
+void *crearSocket(struct Skt *skt,int puerto){
 	char buf[BUFFER_LEN];
 	if ((skt->sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
 		perror("skt");
@@ -38,42 +61,41 @@ void *beginProto(void *sockt){
 
 	/* Se establece la estructura my_addr para luego llamar a bind() */
 	skt->my_addr.sin_family = AF_INET; /* usa host byte order */
-	skt->my_addr.sin_port = htons(SERVER_PORT); /* usa network byte order */
+	skt->my_addr.sin_port = htons(puerto); /* usa network byte order */
 	skt->my_addr.sin_addr.s_addr = INADDR_ANY; /* escuchamos en todas las IPs */
 	bzero(&(skt->my_addr.sin_zero), 8); /* rellena con ceros el resto de la estructura */
 	/* Se le da un nombre al skt (se lo asocia al puerto e IPs) */
 
 	printf("Asignado direccion al skt ....\n");
 	printf("FILE-DESC: %d\n",skt->sockfd);
-	if (bind(skt->sockfd, (struct sockaddr *)&(skt->my_addr), sizeof(struct sockaddr)) == -1) {
+	if (bind(skt->sockfd, (struct sockaddr *)&(skt->my_addr), 
+							 sizeof(struct sockaddr)) == -1) {
 		perror("bind");
 		exit(2);
 	}
-	printf("HOLAA");
-	/* Se reciben los datos (directamente, UDP no necesita conexión) */
-	skt->addr_len = sizeof(struct sockaddr);
-	printf("Esperando datos ....\n");
-	while ((skt->numbytes=recvfrom(skt->sockfd, buf, BUFFER_LEN, 0, (struct sockaddr *)&skt->their_addr,
-	(socklen_t *)&skt->addr_len)) != -1) {
-		/* Se visualiza lo recibido */
-		printf("paquete proveniente de : %s\n",inet_ntoa(skt->their_addr.sin_addr));
-		printf("longitud del paquete en bytes: %d\n",skt->numbytes);
-		buf[skt->numbytes] = '\0';
-		printf("el paquete contiene: %s\n", buf);
-		while (1){
-		}
-	}
-	/* cerramos descriptor del skt */
-	close(skt->sockfd);
 }
+
+//----------------------------------------------------------------------------//
+
+void *beginProtocol(void *buf){
+	char *buffer = buf;
+	printf("el paquete contiene: %s\n", buffer);
+}
+
+//----------------------------------------------------------------------------//
+//                       Inicio del código principal                          //
+//----------------------------------------------------------------------------//
 
 int main(int argc, char *argv[]){
 
-/* Para leer los argumentos cuando se ejecute el programa del servidor
 	int i;
 	long puerto;
-	char entradas[50];
+	char entradas[50]; /* HAY QUE DEFINIR EL TAMANO DEL STRING DE LA RUTA DEL ARCHIVO */
 	char salidas[50];
+	if (argc < 7){
+		printf("\nUso: sem_srv -l puerto_sem_svr -i bitacora_entrada -o bitacora_salida\n\n");
+		exit(0);
+	}
 	for (i=1;i<argc;i++){
 		if (strcmp(argv[i],"-l") == 0){
 			puerto = atoll(argv[i+1]);
@@ -83,89 +105,35 @@ int main(int argc, char *argv[]){
 			strcpy(salidas,argv[i+1]);
 		}
 	}
-	printf("%ld",puerto);
-	printf("%s",entradas);
-	printf("%s",salidas);
-*/
 
-	struct Skt skt1;
-	struct Skt skt2;
-	struct Skt skt3;
-
-	struct Skt sockets[3];
-	sockets[0] = skt1;
-	sockets[1] = skt2;
-	sockets[2] = skt3;
-
+	struct Skt skt;
+	crearSocket(&skt,puerto);
+	char buf[BUFFER_LEN];
 	pthread_t threads[NUM_THREADS];
+	int num_hilos = 0;
 	int rc;
-	long t;
-	for(t=0; t<NUM_THREADS; t++){
-		printf("In main: creating thread %ld\n", t);
-		rc = pthread_create(&threads[t], NULL, beginProto, &sockets[t]);
+	skt.addr_len = sizeof(struct sockaddr);
+	printf("Esperando datos ....\n");
+	while ((skt.numbytes=recvfrom(skt.sockfd, buf, BUFFER_LEN, 0, 
+						(struct sockaddr *)&(skt.their_addr), 
+						(socklen_t *)&(skt.addr_len))) != -1) {
+		buf[skt.numbytes] = '\0';
+		rc = pthread_create(&threads[num_hilos], NULL, beginProtocol, &buf);
 		if (rc){
 			printf("ERROR; return code from pthread_create() is %d\n", rc);
 			exit(-1);
 		}
+		num_hilos = num_hilos + 1;
+		if (num_hilos == 2){
+			num_hilos = 0;
+		}
 	}
-
+	/* cerramos descriptor del skt */
+	close(skt.sockfd);
 	pthread_exit(NULL);
 	exit (0);
 }
 
-/*
-
-Mensajes:
-
-CLIENTE --> SERVIDOR:
-* Al pisar boton: Solicitud de puesto. DATOS: ID vehiculo, Tipo operacion (e)
-* Al pasar el ticket: Calculo de la tarifa. DATOS: ID vehiculo, Tipo operacion (s)
-
----------------------------------------------------------------------------
-|	TIPO OPERACION	|						DATOS						  |
----------------------------------------------------------------------------
-
-Entrada
----------------------------------------------------------------------------
-|	0	|XXXXXXX														  |
----------------------------------------------------------------------------
-
-Salida
----------------------------------------------------------------------------
-|	1	|XXXXXXX														  |
----------------------------------------------------------------------------
-
-
-
-SERVIDOR --> CLIENTE:
-* Permitiendo el paso del cliente. DATOS: Aprobado (1), Hora, fecha y codigo.
-* Denegando la entrada por falta de puestos. DATOS: No aprobado (0) el paso.
-* Monto a cancelar calculado segun el ID y el tiempo de estadia. DATOS: Salida (2), Tarifa
-
----------------------------------------------------------------------------
-|	TIPO MENSAJE	|						DATOS						  |
----------------------------------------------------------------------------
-
----------------------------------------------------------------------------
-|		0		|						VACIO							  |
----------------------------------------------------------------------------
-
-Mensaje de aprobacion de entrada de un vehiculo con placa AAAHC51 a las 00:15:25 del dia 05/10/2016
----------------------------------------------------------------------------
-|		1		|0510201600152512547									  |
----------------------------------------------------------------------------
-
-Mensaje de salida de vehiculo con un monto de 150 (minimo 00000, maximo 9999 -casi 10 mil-)
----------------------------------------------------------------------------
-|		2		|0150													  |
----------------------------------------------------------------------------
-
-
-
-- Estructura
-- Tamano campos seccion datos mensaje
-- Argumentos cliente
-- 
-
-
-*/
+//----------------------------------------------------------------------------//
+//                         Fin del código principal                           //
+//----------------------------------------------------------------------------//
